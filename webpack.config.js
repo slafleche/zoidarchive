1503f5a9ce6e9e6431c67eae8597b5de49798472
slafleche <your_donotreply_email_id>
2022-02-22 15:47:06 -0500

    Added prettier config

--- webpack.config.js
+++ webpack.config.js
@@ -1,73 +1,80 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const FileManagerPlugin = require('filemanager-webpack-plugin');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const FileManagerPlugin = require("filemanager-webpack-plugin");
+const PrettierPlugin = require("prettier-webpack-plugin");
 
 // return env specific values, defaults to 'production'
 const setEnvironmentVars = () => {
-	switch (process.env.APP_ENV) {
-		case 'dev':
-			return {
-				watch: true,
-				filename: '[name]',
-			}
-		default:
-			return {
-				watch: false,
-				filename: '[name].[hash]',
-			}
-	}
-}
-
+  switch (process.env.APP_ENV) {
+    case "dev":
+      return {
+        watch: true,
+        filename: "[name]",
+      };
+    default:
+      return {
+        watch: false,
+        filename: "[name].[hash]",
+      };
+  }
+};
 
 const exportsInit = () => {
   const env = setEnvironmentVars();
   return {
     watch: env.watch,
     entry: [
-      path.resolve('src', 'js', 'app.js'),
-      path.resolve('src', 'styles', 'app.css'),
+      path.resolve("src", "js", "app.js"),
+      path.resolve("src", "styles", "app.css"),
     ],
     output: {
-      path: path.resolve('static', 'assets'),
-      filename: 'bundle.js',
+      path: path.resolve("static", "assets"),
+      filename: "bundle.js",
     },
     module: {
       rules: [
         {
           test: /\.js$/,
           exclude: /node_modules/,
-          use: ['babel-loader'],
+          use: ["babel-loader"],
         },
         {
           test: /\.css$/,
-          use: [
-            MiniCssExtractPlugin.loader,
-            'css-loader',
-            'postcss-loader',
-          ],
+          use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
         },
       ],
     },
     plugins: [
       new MiniCssExtractPlugin({
-        filename: env.filename + '.css',
+        filename: env.filename + ".css",
       }),
       new FileManagerPlugin({
         events: {
           onStart: {
-            delete: [{
-              source: path.resolve('static', 'assets', '*.js'),
-            },{
-              source: path.resolve('static', 'assets', '*.css'),
-              }, {
-                source: path.resolve('public', '*'),
-              }],
+            delete: [
+              {
+                source: path.resolve("static", "assets", "*.js"),
+              },
+              {
+                source: path.resolve("static", "assets", "*.css"),
+              },
+              {
+                source: path.resolve("public", "*"),
+              },
+            ],
           },
-        }
+        },
+      }),
+      new PrettierPlugin({
+        printWidth: 120,
+        useTabs: false,
+        tabWidth: 4,
+        semi: true,
+        trailingComma: "all",
+        encoding: "utf-8",
       }),
     ],
   };
 };
-
 
 module.exports = exportsInit();
