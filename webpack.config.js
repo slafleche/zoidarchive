4f7314c14913a0c54c1db7d48278a719191d018d
slafleche <your_donotreply_email_id>
2022-02-25 14:56:04 -0500

    Deleted unused config Added manifest Fixed env variables in webpack scripts Updated libraries

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,45 @@
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const FileManagerPlugin = require("filemanager-webpack-plugin");
+const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
 
-// Dynamic value for dev and production environments
-const setEnvironmentVars = () => {
-  switch (process.env.APP_ENV) {
-    case "dev":
-      return {
-        watch: true,
-        filename: "[name]",
-      };
-    default:
-      return {
-        watch: false,
-        filename: "[name].[hash]",
-      };
-  }
-};
 
-const exportsInit = () => {
-  const env = setEnvironmentVars();
+
+const exportsInit = (conf) => {
   return {
-    watch: env.watch,
+    watch: conf.watch,
+    devtool: conf.devtool,
     entry: [
       path.resolve("src", "js", "app.js"),
-      path.resolve("src", "styles", "app.css"),
+      // path.resolve("src", "styles", "app.css"),
     ],
     output: {
       path: path.resolve("static", "assets"),
-      filename: "bundle.js",
+      filename: `${conf.filename}.js`,
     },
     module: {
       rules: [
         {
-          test: /\.js$/,
+          test: /\.(ts|js)$/,
           exclude: /node_modules/,
           use: ["babel-loader"],
         },
         {
           test: /\.css$/,
+          exclude: /node_modules/,
           use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
         },
+        {
+          test: /\.(png|jpg|gif|svg)$/,
+          loader: 'file-loader',
+          options: {
+            outputPath: 'assets/'
+            
+          }
+        }
       ],
     },
     plugins: [
-      new MiniCssExtractPlugin({
-        filename: env.filename + ".css",
-      }),
       // Delete old files (prod files are deleted with dev files to avoid confusion)
       new FileManagerPlugin({
         events: {
@@ -54,10 +47,7 @@ const exportsInit = () => {
             {
               delete: [
                 {
-                  source: path.resolve("static", "assets", "*"), // Site code
-                },
-                {
-                  source: path.resolve("static", "libs", "*"), // Third party libraries
+                  source: path.resolve("assets", "*"), // Site code
                 },
                 {
                   source: path.resolve("public", "*"), // Generated Site
@@ -67,8 +57,40 @@ const exportsInit = () => {
           ],
         },
       }),
+      // Create manifest with hashed generated files
+      new WebpackManifestPlugin({
+        fileName : "../../data/manifest.json",
+         writeToFileEmit: true,
+         basePath: 'static/',
+      }),
+      new MiniCssExtractPlugin({
+        filename: conf.filename,
+      }),
     ],
   };
 };
 
-module.exports = exportsInit();
+
+// Dynamic value for dev and production environments
+const setEnvConfig = (mode) => {
+  switch (mode) {
+    case "development":
+      console.log("\n=====> DEV CONFIG\n");
+      return {
+        watch: true,
+        filename: "[name]",
+        devtool: "source-map",
+      };
+    default:
+      console.log("\n=====> PROD CONFIG\n");
+      return {
+        watch: false,
+        filename: "[name].[fullhash]",
+        devtool: "inline-source-map",
+      };
+  }
+};
+
+module.exports = (env, argv) => {
+  return exportsInit(setEnvConfig(argv.mode));
+}
