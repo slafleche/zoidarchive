29d7ca994c76bac945cf8bb97064cacc7c2ddab8
Stéphane LaFlèche <19651440+slafleche@users.noreply.github.com>
2022-02-27 22:06:03 -0500

    Working on webpack scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,75 +1,79 @@
 const path = require("path");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const FileManagerPlugin = require("filemanager-webpack-plugin");
-const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
-
-
+const { WebpackManifestPlugin } = require("webpack-manifest-plugin");
 
 const exportsInit = (conf) => {
   return {
     watch: conf.watch,
     devtool: conf.devtool,
     entry: [
-      path.resolve("src", "js", "app.js"),
+      path.resolve("src", "index.ts"),
       // path.resolve("src", "styles", "app.css"),
     ],
     output: {
-      path: path.resolve("static", "assets"),
+      path: path.resolve("assets", "js"),
       filename: `${conf.filename}.js`,
     },
     module: {
       rules: [
         {
-          test: /\.(ts|js)$/,
+          test: /\.(ts|tsx)$/,
           exclude: /node_modules/,
           use: ["babel-loader"],
         },
-        {
-          test: /\.css$/,
-          exclude: /node_modules/,
-          use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
-        },
-        {
-          test: /\.(png|jpg|gif|svg)$/,
-          loader: 'file-loader',
-          options: {
-            outputPath: 'assets/'
-            
-          }
-        }
+        // {
+        //   test: /\.css$/,
+        //   exclude: /node_modules/,
+        //   use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+        // },
+        // {
+        //   test: /\.(png|jpg|gif|svg)$/,
+        //   loader: 'file-loader',
+        //   options: {
+        //     outputPath: 'assets/'
+
+        //   }
+        // }
       ],
     },
     plugins: [
-      // Delete old files (prod files are deleted with dev files to avoid confusion)
+      // // Delete old files (prod files are deleted with dev files to avoid confusion)
       new FileManagerPlugin({
         events: {
           onStart: [
             {
               delete: [
                 {
-                  source: path.resolve("assets", "*"), // Site code
+                  source: path.resolve("assets", "css", "*"),
                 },
                 {
-                  source: path.resolve("public", "*"), // Generated Site
+                  source: path.resolve("assets", "js", "*"),
+                },
+                {
+                  source: path.resolve("_data", "manifest.json"),
                 },
               ],
             },
           ],
         },
       }),
-      // Create manifest with hashed generated files
+      // // Create manifest with hashed generated files
       new WebpackManifestPlugin({
-        fileName : "../../data/manifest.json",
-         writeToFileEmit: true,
-         basePath: 'static/',
+        fileName: "../../_data/manifest.json",
+        writeToFileEmit: true,
+        basePath: "",
+        map: (f) => {
+          f.path = f.path.replace(/^auto\//, "");
+          return f;
+        },
       }),
-      new MiniCssExtractPlugin({
-        filename: conf.filename,
-      }),
+      // new MiniCssExtractPlugin({
+      //   filename: conf.filename,
+      // }),
     ],
   };
 };
-
 
 // Dynamic value for dev and production environments
 const setEnvConfig = (mode) => {
@@ -93,4 +97,4 @@ const setEnvConfig = (mode) => {
 
 module.exports = (env, argv) => {
   return exportsInit(setEnvConfig(argv.mode));
-}
+};
