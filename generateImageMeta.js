fc08e3f1c111f0d2f6385ab619054bcfebe3edb4
Stéphane LaFlèche <19651440+slafleche@users.noreply.github.com>
2022-04-21 15:05:10 -0400

    Added getImageMeta function to get image sizes for NodeJS

---
+++ generateImageMeta.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+const path = require("path");
+const sizeOf = require("image-size");
+
+const getImageData = (dirPath, truncateStart, arrayOfFiles) => {
+  files = fs.readdirSync(dirPath);
+  arrayOfFiles = arrayOfFiles || [];
+  files.forEach(function (file) {
+    const currentItem = path.join(dirPath, file);
+    if (fs.statSync(currentItem).isDirectory()) {
+      arrayOfFiles = getImageData(currentItem, truncateStart, arrayOfFiles);
+    } else {
+      if (file.match(/\.(jpg|jpeg|png|gif|webp)$/gi)) {
+        const relativePath = currentItem.substring(
+          truncateStart,
+          currentItem.length
+        );
+
+        const data = sizeOf(currentItem);
+        arrayOfFiles.push({
+          [relativePath]: data,
+        });
+      }
+    }
+  });
+  return arrayOfFiles;
+};
+
+const writeToFile = (data) => {
+  fs.writeFile("src/meta/imageData.js", data, function (err) {
+    if (err) return console.log(err);
+  });
+};
+
+// Run Script
+const dirPath = path.join(process.cwd(), "public/images/");
+const relativePathOffset = (process.cwd() + "public").length + 1;
+const imageData = getImageData(dirPath, relativePathOffset, []);
+writeToFile(`export const imageMeta = ${JSON.stringify(imageData)};`);
